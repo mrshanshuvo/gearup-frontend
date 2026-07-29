e502ec8eb2ef147059f6eee99afcc4cbd1f5c85c
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2026-07-29 21:22:40 +0600

    fix: add suppressHydrationWarning to body tag for browser extension compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="flex min-h-full flex-col">
+      <body className="flex min-h-full flex-col" suppressHydrationWarning>
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
